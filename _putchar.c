942d01326a2c1cfd50b460b8d79ebd81e8e7306f
korede thompson <koredeodeyemi456@gmail.com>
2023-10-15 08:18:46 +0000

    header file and _putchar file

---
+++ _putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _putchar - it prints char to the stdout
+ * @c: char to be printed
+ * Return: 0 always
+ */
+
+int _putchar(int c)
+{
+	return (fputc(c, stdout))
+}
---
+++ main.h
@@ -0,0 +1,31 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+typedef struct t_printf
+{
+	char *spec_c;
+	int (*func)(va_list);
+} t_printf;
+
+int _putchar(int c);
+int _printf(const char *format, ...);
+int print_char(va_list);
+int print_num(va_list);
+int print_int(va_list);
+int print_str(va_list);
+int print_percent(va_list);
+int print_binary(va_list);
+int print_reversed(va_list);
+int rot13(va_list);
+int unsigned_integer(va_list);
+int print_octal(va_list);
+int print_hex(va_list);
+int print_HEX(va_list);
+int print_address(va_list;
+int select_f(const char *format, t_printf func_arr[], va_list arg_list);
+
+#endif
